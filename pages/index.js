75be125516e958ad05a4d2440512391a611b8a06
Shoaib-ali7860 <54176556+Shoaib-ali7860@users.noreply.github.com>
2021-02-11 12:30:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,71 @@
 import Head from 'next/head'
+import ShowCountries from './showCountries'
 import styles from '../styles/Home.module.css'
+// import Countries from './countries.js'
+import {useState} from 'react'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-           hii its works
-    </div>
-  )
+import React from 'react'
+
+
+
+
+
+
+
+function Index({comm}) {
+
+  const [keyword,setKeyword]=useState("")
+const filterContries=comm.filter(country=>country.name.toLowerCase().includes(keyword))
+
+const OnDataChange=(e)=>{
+  e.preventDefault()
+  console.log(e.target.value.toLowerCase())
+  setKeyword(e.target.value.toLowerCase())
 }
+
+  return (<div className={styles.container}>
+       <h1>Countries</h1>
+       <input type="text" onChange={OnDataChange} placeholder="enter Country name"/>
+    <ShowCountries country={filterContries}/>
+
+<style jsx>{`
+   {
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 20px;
+}
+
+   }
+  `}</style>
+    </div>)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//=====================================================================================
+Index.getInitialProps = async (ctx) => {
+
+ 
+
+const res = await fetch('https://restcountries.eu/rest/v2/all')
+    const comm = await res.json()
+
+
+    
+    return {comm}
+  
+}
+
+
+export default Index
