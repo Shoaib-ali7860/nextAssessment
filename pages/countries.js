56a70ee848a8c0d4e7fa83a4ea8fd3d882d7763c
Shoaib-ali7860 <54176556+Shoaib-ali7860@users.noreply.github.com>
2021-02-11 19:48:30 +0530

    Update countries.js

--- pages/countries.js
+++ pages/countries.js
@@ -1,42 +1,56 @@
-
+// import fetch from "isomorphic-fetch"
+import React from 'react'
+import BorderCountries from './borderCountries'
+import CountryDescription from './countryDescription'
 import styles from '../styles/Home.module.css'
+import {useEffect,useState} from 'react'
 
-function BorderImageCard({border})
-{
-   return (
-       <div >
-        <img src={border.country1.flag} width="200px" height="120px" style={{margin:"10px"}}/>
-        
-         
-       </div>
-        
+const getCountry=async (title)=>{
 
-   )
+     const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${title}`)
+    const country1 = await res.json()
+    return {country1}
 
 }
-
-
-function BorderCountries({borders}){
-
-if(!borders){
-  return null
+function Countries({country1}){
+const [borders,setBorders]=useState([])
+const getBorder= async () =>{
+	 const borders=await Promise.all(country1.borders.map(border=>getCountry(border)))
+	 setBorders(borders)
 }
 
-return (
-	<div className={styles.borderContainer}>
-     <h3>Neighbour Countries</h3>
-   <div className={styles.grid}>
+useEffect(()=>{
 
-       {
- 
-	borders.map(border=><BorderImageCard key={border.country1.name} border={border}/>)
-           }
-   </div>
+	getBorder()
+},[])
+// const {name}=country1
 
-   </div>
-	)
+   // const borders=country1.borders
+  
+
+
+	return <div className={styles.container}>
+<CountryDescription country1={country1}/>
+<BorderCountries borders={borders}/>
+	</div>
+
+
 
 }
+Countries.getInitialProps=async ({query})=> {
+  // const res = await fetch(`https://restcountries.eu/rest/v2/alpha/irn`)
+    const country1 = await getCountry(query.title)
 
 
-export default BorderCountries
+    
+    return country1
+}
+
+   
+
+
+    
+  
+
+
+export default Countries 
